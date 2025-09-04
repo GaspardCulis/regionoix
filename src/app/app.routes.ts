ccda37fe4583a0e1bbb39bc61af51b659199d7de
delachal <laurine.delachapelle@gmail.com>
2025-09-04 09:23:55 +0200

    feat[front] : showcase page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { TestComponent } from './component/test-component/test-component';
+import { Showcase } from './pages/showcase/showcase-page';
 
 export const routes: Routes = [
-    {path: '', component: TestComponent },
-    {path: '**', redirectTo: '' }
+    { path: '', component: TestComponent },
+    { path: '**', redirectTo: '' },
+
+    { path: 'showcase', component: Showcase }
 ];
---
+++ src/app/pages/showcase/showcase-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-showcase',
+  imports: [],
+  templateUrl: './showcase-page.html',
+  styleUrl: './showcase-page.css'
+})
+export class Showcase {
+
+}
