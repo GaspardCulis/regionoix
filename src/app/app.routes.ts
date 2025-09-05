e0430d083ef414b54d47226bae6b7bac26541916
delachal <laurine.delachapelle@gmail.com>
2025-09-05 15:35:09 +0200

    improve(front): renaming connexion to connection and change english words in front to french

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { ShowcasePage } from './pages/showcase/showcase-page';
 import { BasketPage } from './pages/basket/basket-page';
 import { PaymentPage } from './pages/payment/payment-page';
+import { ConnectionPage } from './pages/connection-page/connection-page';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'showcase', pathMatch: 'full' },
     { path: 'showcase', component: ShowcasePage },
     { path: 'basket', component: BasketPage },
-    { path: 'payment', component: PaymentPage }
+    { path: 'payment', component: PaymentPage },
+    { path: 'connection', component: ConnectionPage }
 ];
