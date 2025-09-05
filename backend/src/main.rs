5bcb6e693163fb516ee95ae864e52bde45ec8368
GaspardCulis <gaspard@gasdev.fr>
2025-09-05 15:41:34 +0200

    refactor(backend): Better loading of secret env vars

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,12 +1,12 @@
 use actix_identity::IdentityMiddleware;
 use actix_session::{SessionMiddleware, storage::RedisSessionStore};
 use actix_web::{App, HttpServer, cookie::Key, web};
-use dotenv::dotenv;
 use sea_orm::{Database, DatabaseConnection};
 
 pub mod entities;
 mod error;
 mod routes;
+mod secrets;
 
 pub use error::*;
 
@@ -17,23 +17,17 @@ struct AppState {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Loading environment variables from .env");
-    dotenv().ok();
-
-    let api_host = std::env::var("API_HOST").expect("API_HOST must be set.");
-    let api_port = std::env::var("API_PORT").expect("API_PORT must be set.");
-    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
-    let redis_url = std::env::var("REDIS_URL").expect("REDIS_URL must be set.");
-    let secret_key = std::env::var("SECRET_KEY").expect("SECRET_KEY must be set.");
+    let secrets = secrets::Secrets::load().expect("load secrets");
 
     println!("Connecting to database");
-    let db = Database::connect(database_url)
+    let db = Database::connect(secrets.database_url)
         .await
         .expect("Failed to connect to database");
 
     println!("Connected to database");
 
     println!("Connecting to Redis session store");
-    let redis_store = RedisSessionStore::new(redis_url).await.unwrap();
+    let redis_store = RedisSessionStore::new(secrets.redis_url).await.unwrap();
     println!("Connected to Redis session store");
 
     HttpServer::new(move || {
@@ -41,15 +35,12 @@ async fn main() -> std::io::Result<()> {
             .wrap(IdentityMiddleware::default())
             .wrap(SessionMiddleware::new(
                 redis_store.clone(),
-                Key::from(secret_key.as_bytes()),
+                Key::from(secrets.secret_key.as_bytes()),
             ))
             .app_data(web::Data::new(AppState { db: db.clone() }))
             .service(web::scope("/api").configure(routes::config))
     })
-    .bind((
-        api_host,
-        api_port.parse().expect("Failed to parse API_PORT"),
-    ))?
+    .bind((secrets.api_host, secrets.api_port))?
     .run()
     .await
 }
