5ed089a4e6e9fdbc01d9e1b1650a87f1087753aa
Anna Clavelier <anna.clavelier@etu.univ-grenoble-alpes.fr>
2025-09-05 10:45:22 +0200

    feat(backend): Add read a product by id

--- backend/src/routes/products.rs
+++ backend/src/routes/products.rs
@@ -1,4 +1,4 @@
-use actix_web::{HttpResponse, Responder, get, web};
+use actix_web::{HttpRequest, HttpResponse, Responder, get, web};
 use sea_orm::EntityTrait as _;
 
 use crate::{
@@ -8,9 +8,10 @@ use crate::{
 
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(get);
+    cfg.service(get_by_id);
 }
 
-#[get("/")]
+#[get("")]
 pub async fn get(data: web::Data<AppState>) -> impl Responder {
     let db = &data.db;
     let products: Vec<product::Model> = Product::find()
@@ -19,4 +20,21 @@ pub async fn get(data: web::Data<AppState>) -> impl Responder {
         .expect("Failed to get products");
 
     HttpResponse::Ok().json(products)
+}
+
+#[get("/{id}")]
+pub async fn get_by_id(data: web::Data<AppState>, req: HttpRequest) -> impl Responder {
+    let db = &data.db;
+    let id: u8 = req.match_info().query("id").parse().unwrap();
+    println!("id sent {0}", id);
+    let product: Option<product::Model> = Product::find_by_id(id)
+        .one(db)
+        .await
+        .expect(&format!("Failed to get product of id  {}", id));
+
+    if product.is_none() {
+        todo!("Throw 404 error")
+    }
+
+    HttpResponse::Ok().json(product)
 }
