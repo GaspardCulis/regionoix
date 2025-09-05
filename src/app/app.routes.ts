c5e03ef764a07cb49e2b5d53b58215dfb2b5ed08
delachal <laurine.delachapelle@gmail.com>
2025-09-05 10:29:28 +0200

    feat(front):creation of basket page, redirection from topbar basket logo, naming convention correction

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
-import { Showcase } from './pages/showcase/showcase.page';
+import { ShowcasePage } from './pages/showcase/showcase-page';
+import { BasketPage } from './pages/basket/basket-page';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'showcase', pathMatch: 'full' },
-    { path: 'showcase', component: Showcase }
+    { path: 'showcase', component: ShowcasePage },
+    { path: 'basket', component: BasketPage }
 ];
