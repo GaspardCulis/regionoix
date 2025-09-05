0ee27e8138af78236b72022d17ac8e8f14138922
GaspardCulis <gaspard@gasdev.fr>
2025-09-05 14:38:34 +0200

    feat(backend): Add auth routes with better error handling

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,8 +1,14 @@
-use actix_web::{App, HttpServer, web};
+use actix_identity::IdentityMiddleware;
+use actix_session::{SessionMiddleware, storage::RedisSessionStore};
+use actix_web::{App, HttpServer, cookie::Key, web};
 use dotenv::dotenv;
-pub mod entities;
-mod routes;
 use sea_orm::{Database, DatabaseConnection};
+
+pub mod entities;
+mod error;
+mod routes;
+
+pub use error::*;
 
 struct AppState {
     db: DatabaseConnection,
@@ -12,9 +18,12 @@ struct AppState {
 async fn main() -> std::io::Result<()> {
     println!("Loading environment variables from .env");
     dotenv().ok();
+
     let api_host = std::env::var("API_HOST").expect("API_HOST must be set.");
     let api_port = std::env::var("API_PORT").expect("API_PORT must be set.");
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
+    let secret_key = std::env::var("SECRET_KEY").expect("SECRET_KEY must be set.");
+
     println!("Connecting to database");
     let db = Database::connect(database_url)
         .await
@@ -22,8 +31,19 @@ async fn main() -> std::io::Result<()> {
 
     println!("Connected to database");
 
+    println!("Connecting to Redis session store");
+    let redis_store = RedisSessionStore::new("redis://127.0.0.1:6379")
+        .await
+        .unwrap();
+    println!("Connected to Redis session store");
+
     HttpServer::new(move || {
         App::new()
+            .wrap(IdentityMiddleware::default())
+            .wrap(SessionMiddleware::new(
+                redis_store.clone(),
+                Key::from(secret_key.as_bytes()),
+            ))
             .app_data(web::Data::new(AppState { db: db.clone() }))
             .service(web::scope("/api").configure(routes::config))
     })
---
+++ backend/src/routes/auth/mod.rs
@@ -0,0 +1,78 @@
+use crate::Result;
+use actix_identity::Identity;
+use actix_web::{HttpMessage as _, HttpRequest, HttpResponse, Responder, get, post, web};
+use argon2::{Argon2, PasswordHash, PasswordVerifier as _};
+use sea_orm::{EntityName, EntityTrait as _};
+use serde::{Deserialize, Serialize};
+
+use crate::{AppState, entities::user};
+
+pub fn config(cfg: &mut web::ServiceConfig) {
+    cfg.service(login).service(logout).service(status);
+}
+
+#[derive(Deserialize)]
+pub struct LoginRequest {
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct JwtClaims {
+    /// User id
+    sub: i32,
+    /// Expiration time
+    exp: usize,
+}
+
+#[post("/login")]
+pub async fn login(
+    request: HttpRequest,
+    login_request: web::Json<LoginRequest>,
+    data: web::Data<AppState>,
+) -> Result<HttpResponse> {
+    let db = &data.db;
+    let users: Vec<user::Model> = user::Entity::find().all(db).await?;
+
+    let user = match users
+        .into_iter()
+        .find(|user| user.email == login_request.email)
+    {
+        Some(user) => Ok(user),
+        None => Err(crate::Error::EntityNotFound {
+            table_name: user::Entity.table_name(),
+        }),
+    }?;
+
+    check_password(&login_request, &user)?;
+
+    Identity::login(&request.extensions(), format!("{}", user.id)).unwrap();
+
+    Ok(HttpResponse::Ok().finish())
+}
+
+#[post("/logout")]
+async fn logout(user: Option<Identity>) -> impl Responder {
+    if let Some(user) = user {
+        user.logout();
+    }
+    HttpResponse::Ok()
+}
+
+fn check_password(login_request: &LoginRequest, user: &user::Model) -> Result<()> {
+    let parsed_hash = PasswordHash::new(&user.password)
+        .map_err(|err| crate::Error::InternalError(anyhow::Error::msg(err)))?;
+
+    Argon2::default()
+        .verify_password(login_request.password.as_bytes(), &parsed_hash)
+        .map_err(|_| crate::Error::AuthenticationFailure)
+}
+
+#[get("/status")]
+async fn status(user: Option<Identity>) -> impl Responder {
+    if let Some(user) = user {
+        format!("authenticated as {}", user.id().unwrap())
+    } else {
+        "unauthenticated".to_owned()
+    }
+}
