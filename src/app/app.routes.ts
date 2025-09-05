9c98c03a9447a6d752ccdad05534593f16e0341b
delachal <laurine.delachapelle@gmail.com>
2025-09-05 16:37:42 +0200

    feat(front): create product page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { ShowcasePage } from './pages/showcase/showcase-page';
 import { BasketPage } from './pages/basket/basket-page';
 import { PaymentPage } from './pages/payment/payment-page';
 import { ConnectionPage } from './pages/connection-page/connection-page';
+import { ProductPage } from './pages/product-page/product-page';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'showcase', pathMatch: 'full' },
     { path: 'showcase', component: ShowcasePage },
     { path: 'basket', component: BasketPage },
     { path: 'payment', component: PaymentPage },
-    { path: 'connection', component: ConnectionPage }
+    { path: 'connection', component: ConnectionPage },
+    { path: 'product', component: ProductPage }
 ];
---
+++ src/app/pages/product-page/product-page.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-product-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-page.html',
+  styleUrl: './product-page.css'
+})
+export class ProductPage {
+  product = {
+    title: 'Cœur de Celtes Chocolat Noir et Salidou',
+    description:
+      "Découvrez les Biscuits Cœur de Celtes au Chocolat Noir et Salidou Le Cœur de Celtes chocolat noir et Salidou est la parfaite alliance entre tradition bretonne et gourmandise raffinée. Ces biscuits bretons artisanaux sont conçus pour offrir une expérience gustative inégalée. En croquant dans ces délicieuses douceurs, vous serez enveloppé par la richesse du chocolat noir et le fondant du caramel au beurre salé Salidou, une spécialité bretonne emblématique. Leur texture croquante, combinée à un cœur fondant, fait de ces biscuits le choix idéal pour une pause gourmande ou un cadeau exquis à offrir.",
+    image: 'https://picsum.photos/400/300',
+    price: 7.5
+  };
+
+}
