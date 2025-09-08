0d7b19f2db2dbfc08dfcd0ff03ba2dc35979d887
delachal <laurine.delachapelle@gmail.com>
2025-09-08 09:23:58 +0200

    feat(front): add quantity increase/decrease button

--- src/app/pages/product-page/product-page.ts
+++ src/app/pages/product-page/product-page.ts
@@ -14,7 +14,8 @@ export class ProductPage {
     description:
       "Découvrez les Biscuits Cœur de Celtes au Chocolat Noir et Salidou Le Cœur de Celtes chocolat noir et Salidou est la parfaite alliance entre tradition bretonne et gourmandise raffinée. Ces biscuits bretons artisanaux sont conçus pour offrir une expérience gustative inégalée. En croquant dans ces délicieuses douceurs, vous serez enveloppé par la richesse du chocolat noir et le fondant du caramel au beurre salé Salidou, une spécialité bretonne emblématique. Leur texture croquante, combinée à un cœur fondant, fait de ces biscuits le choix idéal pour une pause gourmande ou un cadeau exquis à offrir.",
     image: 'https://picsum.photos/400/300',
-    price: 7.5
+    price: 7.5,
+    quantity: 1,
   };
 
 }
