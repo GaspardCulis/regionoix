8448f61fced66c82985acfd56b0cbf38ef971368
delachal <laurine.delachapelle@gmail.com>
2025-09-09 13:11:55 +0200

    refactor(front): add productService

--- src/app/utils/services/product-service.ts
+++ src/app/utils/services/product-service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 })
 export class ProductService {
 
-  private apiUrl = 'https://www.regionoix.gasdev.fr/api/products';
+  private apiUrl = '/api/products';
   private http = inject(HttpClient);
 
   getProducts(): Observable<Product[]> {
