5395611b27dd26f5ddc69c7ab5ccc776377b34c2
Anna Clavelier <anna.clavelier@etu.univ-grenoble-alpes.fr>
2025-09-09 16:29:52 +0200

    docs(backend/api): Add doc auth routes

--- backend/src/routes/auth.rs
+++ backend/src/routes/auth.rs
@@ -26,7 +26,7 @@ struct JwtClaims {
     exp: usize,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, utoipa::ToSchema)]
 pub struct LoggedUser {
     pub id: i32,
     pub email: String,
@@ -50,7 +50,20 @@ impl FromRequest for LoggedUser {
     }
 }
 
-#[utoipa::path()]
+#[utoipa::path(
+    summary = "Logs user",
+    tag="Authentification", 
+    request_body(content_type = "application/json", content= LoginRequest),
+    responses(
+        (
+            status = 200, 
+            description="Logged in successfully",  
+        ),
+        (
+            status = 404, 
+            description="Email not found",  
+        )
+))]
 #[post("/login")]
 async fn login(
     request: HttpRequest,
@@ -79,7 +92,15 @@ async fn login(
     Ok(HttpResponse::Ok().finish())
 }
 
-#[utoipa::path()]
+#[utoipa::path( 
+    summary = "Logs user out",
+    tag="Authentification", 
+    responses(
+        (
+            status = 200, 
+            description="Logged out successfully",  
+        ),
+))]
 #[post("/logout")]
 async fn logout(user: Option<Identity>) -> impl Responder {
     if let Some(user) = user {
@@ -88,7 +109,16 @@ async fn logout(user: Option<Identity>) -> impl Responder {
     HttpResponse::Ok()
 }
 
-#[utoipa::path()]
+#[utoipa::path(
+    summary = "Returns user currently authentified",
+    tag="Authentification", 
+    responses(
+        (
+            status = 200, 
+            content_type = "Application/Json",
+            body = LoggedUser
+        ),
+))]
 #[get("/status")]
 async fn status(logged_user: LoggedUser) -> impl Responder {
     HttpResponse::Ok().json(logged_user)
