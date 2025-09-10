3f880145604571bcf85d159723690d0c535df947
delachal <laurine.delachapelle@gmail.com>
2025-09-10 15:22:28 +0200

    refactor(front): rename basketUrl

--- src/app/utils/services/basket-service.ts
+++ src/app/utils/services/basket-service.ts
@@ -8,26 +8,26 @@ import { BasketResponse } from '../../models/basket-model';
 })
 export class BasketService {
   private http = inject(HttpClient);
-  private basketUrl = '/api/basket';
+  private readonly apiUrl = '/api/basket';
 
   getBasket(): Observable<BasketResponse> {
-    return this.http.get<BasketResponse>(this.basketUrl);
+    return this.http.get<BasketResponse>(this.apiUrl);
   }
 
   addItem(product_id: number, quantity = 1) {
-    return this.http.post(this.basketUrl + '/items', { product_id, quantity });
+    return this.http.post(this.apiUrl + '/items', { product_id, quantity });
   }
 
   updateItem(product_id: number, quantity: number) {
-    return this.http.patch(this.basketUrl + `/items/${product_id}`, { quantity });
+    return this.http.patch(this.apiUrl + `/items/${product_id}`, { quantity });
   }
 
   removeItem(product_id: number) {
-    return this.http.delete(this.basketUrl + `/items/${product_id}`);
+    return this.http.delete(this.apiUrl + `/items/${product_id}`);
   }
 
   empty() {
-    return this.http.delete(this.basketUrl);
+    return this.http.delete(this.apiUrl);
   }
 
 }
