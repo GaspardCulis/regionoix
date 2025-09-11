de2e9d7466e7e6648225e6b73bf1883a25068624
GaspardCulis <gaspard@gasdev.fr>
2025-09-11 10:32:21 +0200

    chore(backend): Make dtos fields public

--- backend/src/dtos/category.rs
+++ backend/src/dtos/category.rs
@@ -7,7 +7,7 @@ use crate::entities::category;
 #[derive(DerivePartialModel, Serialize, Deserialize, ToSchema, Debug)]
 #[sea_orm(entity = "category::Entity", from_query_result)]
 pub struct CategoryDto {
-    id: i32,
-    name: String,
-    category_parent: Option<i32>,
+    pub id: i32,
+    pub name: String,
+    pub category_parent: Option<i32>,
 }
