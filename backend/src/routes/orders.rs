dd27ddf6e2cb837661953c7b50c0499936f5e625
Anna Clavelier <anna.clavelier@etu.univ-grenoble-alpes.fr>
2025-09-11 11:27:46 +0200

    feat(backend): Add route to get completed orders

--- backend/src/routes/orders.rs
+++ backend/src/routes/orders.rs
@@ -6,12 +6,13 @@ use chrono::Utc;
 use sea_orm::{ColumnTrait, EntityTrait as _, QueryFilter};
 
 pub fn config(cfg: &mut ServiceConfig) {
-    cfg.service(get_in_progress);
+    cfg.service(get_in_progress).service(get_past);
 }
 
 #[utoipa::path(
     summary="Returns order list in progress",
-    tag="Orders in progress for current user",
+    description="Orders in progress for current user",
+    tag="Orders",
     responses(
         (
             status=200,
@@ -39,3 +40,33 @@ pub async fn get_in_progress(
     Ok(HttpResponse::Ok().json(orders))
 }
 
+#[utoipa::path(
+    summary="Returns completed order list",
+    description="Orders completed for current user",
+    tag="Orders",
+    responses(
+        (
+            status=200,
+            description="Order completed list successfully returned",
+            content_type="application/json",
+            body=[OrderDto],
+        ),
+    ),
+)]
+#[get("/past")]
+pub async fn get_past(
+    data: Data<AppState>,
+    logged_user: LoggedUser,
+) -> crate::Result<HttpResponse> {
+    let db = &data.db;
+    let today = Utc::now().date_naive();
+
+    let orders: Vec<OrderDto> = Order::find()
+        .filter(order::Column::UserId.eq(logged_user.id))
+        .filter(order::Column::ArrivalDate.lt(today))
+        .into_dto()
+        .all(db)
+        .await?;
+
+    Ok(HttpResponse::Ok().json(orders))
+}
