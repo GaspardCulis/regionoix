2fba0c2cbe4fc53c2cdb74faaeea87c4fe37b996
GaspardCulis <gaspard@gasdev.fr>
2025-09-11 13:48:26 +0200

    feat(backend/search): Add sorting parameters and implementation

--- backend/src/routes/search.rs
+++ backend/src/routes/search.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use actix_web::web::Query;
 use regionoix::{
     dtos::{product::ProductDto, product_index::ProductIndex},
@@ -20,6 +22,10 @@ struct SearchQuery {
     /// The list of filterable attributes is `["weight", "price", "categories", "tags"]`.
     /// See the [Meilisearch filter expression reference](https://www.meilisearch.com/docs/learn/filtering_and_sorting/filter_expression_reference#filter-expression-reference) for more info.
     filters: Option<String>,
+    /// Sort by some specific attribute in the format `attribute:method` where `method: asc | desc` .
+    /// Ex: `price:asc`.
+    /// See the [Meilisearch sorting API](https://www.meilisearch.com/docs/reference/api/search#sort) for more info.
+    sort: Option<String>,
 }
 
 #[utoipa::path(
@@ -45,18 +51,21 @@ async fn search(query: Query<SearchQuery>, data: Data<AppState>) -> crate::Resul
         .search()
         .with_query(&query.query)
         .with_filter(query.filters.as_ref().unwrap_or(&String::new()))
+        .with_sort(&[query.sort.as_ref().unwrap_or(&String::new()).as_str()])
         .execute::<ProductIndex>()
         .await
         .map_err(|e| anyhow::Error::from(e))?;
 
     let ids: Vec<_> = results.hits.iter().map(|hit| hit.result.id).collect();
+    let id_to_order: HashMap<_, _> = ids.iter().enumerate().map(|(i, &id)| (id, i)).collect();
 
-    // PERF: Using `IS IN` might be sub optimal performanc-wise for large amount of search results
-    let product_results = Product::find()
-        .filter(product::Column::Id.is_in(ids))
+    let mut product_results = Product::find()
+        .filter(product::Column::Id.is_in(ids.clone()))
         .into_dto::<ProductDto>()
         .all(db)
         .await?;
+
+    product_results.sort_by_key(|p| id_to_order[&p.id]);
 
     Ok(HttpResponse::Ok().json(product_results))
 }
