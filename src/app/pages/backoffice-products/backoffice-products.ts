e01774db6f3c2307d2a55e3e9796e559e48dc182
annaclavelier <anna.clavelier@etu.univ-grenoble-alpes.fr>
2025-09-16 15:24:32 +0200

    feat(front): Delete product from backoffice

--- src/app/pages/backoffice-products/backoffice-products.ts
+++ src/app/pages/backoffice-products/backoffice-products.ts
@@ -2,7 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { Product } from '../../models/product-model';
 import { ProductService } from '../../services/product-service';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faCircle, faCircleExclamation, faPlus } from '@fortawesome/free-solid-svg-icons';
 import { Router } from '@angular/router';
 import { SnackbarService } from '../../services/snackbar-service';
 
@@ -15,13 +15,19 @@ import { SnackbarService } from '../../services/snackbar-service';
 export class BackofficeProducts implements OnInit {
   // font awesome icon plus
   faPlus = faPlus;
+  faCircleExclamation = faCircleExclamation;
 
   products: Product[] = [];
+  selectedProduct: Product | null = null;
   private readonly productService = inject(ProductService);
   private readonly router = inject(Router);
   private readonly snackBar = inject(SnackbarService);
 
   ngOnInit(): void {
+    this.loadProducts();
+  }
+
+  loadProducts(): void {
     this.productService.getProducts().subscribe({
       next: (data) => (this.products = data),
       error: (err) => {
@@ -41,6 +47,35 @@ export class BackofficeProducts implements OnInit {
 
   onSee(id: number): void {
     this.router.navigate(['/backoffice/products/', id]);
+  }
+
+  openDeleteModal(product: Product): void {
+    this.selectedProduct = product;
+    const modal: any = document.getElementById('delete_modal');
+    if (modal) modal.showModal();
+  }
+
+  deleteProduct(): void {
+    if (!this.selectedProduct) return;
+    //TODO uncomment
+    /*
+    this.productService.delete(this.selectedProduct.id).subscribe({
+      next: () => {
+        this.snackBar.show(
+          `Le produit "${this.selectedProduct?.name}" a bien été supprimé`,
+          'success'
+        );
+        // Update products
+        this.loadProducts();
+        this.selectedProduct = null;
+      },
+      error: () => {
+        this.snackBar.show(
+          'Échec lors de la suppression du produit',
+          'error'
+        );
+      },
+    });*/
   }
 
 }
