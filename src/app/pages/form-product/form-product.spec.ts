af1a4d11ac51c21b6ef8ef5c41de1ec8e4b608fb
delachal <laurine.delachapelle@gmail.com>
2025-09-17 16:27:12 +0200

    test(front): add minimum tests for BO-product BO-products form-product and admin-menu

--- src/app/pages/form-product/form-product.spec.ts
+++ src/app/pages/form-product/form-product.spec.ts
@@ -1,6 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { FormProduct } from './form-product';
+import { of } from 'rxjs';
+import { Router } from '@angular/router';
+import { CategoriesService, BrandsService, TagsService, RegionsService, AdminService } from '../../generated/clients/regionoix-client';
+import { SnackbarService } from '../../services/snackbar-service';
+
+class MockCategoriesService { get() { return of([]); } }
+class MockBrandsService { get() { return of([]); } }
+class MockTagsService { get() { return of([]); } }
+class MockRegionsService { get() { return of([]); } }
+class MockAdminService { upload(file: File, meta: any) { return of(null); } }
+class MockSnackbarService { show(message: string, type: string) { } }
 
 describe('FormProduct', () => {
   let component: FormProduct;
@@ -8,9 +18,17 @@ describe('FormProduct', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FormProduct]
-    })
-    .compileComponents();
+      imports: [FormProduct],
+      providers: [
+        { provide: CategoriesService, useClass: MockCategoriesService },
+        { provide: BrandsService, useClass: MockBrandsService },
+        { provide: TagsService, useClass: MockTagsService },
+        { provide: RegionsService, useClass: MockRegionsService },
+        { provide: AdminService, useClass: MockAdminService },
+        { provide: SnackbarService, useClass: MockSnackbarService },
+        { provide: Router, useValue: { navigate: jasmine.createSpy('navigate') } }
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(FormProduct);
     component = fixture.componentInstance;
--- src/app/utils/component/admin-menu-component/admin-menu.spec.ts
+++ src/app/utils/component/admin-menu-component/admin-menu.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AdminMenu } from './admin-menu';
+import { RouterLink } from '@angular/router';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AdminMenu', () => {
   let component: AdminMenu;
@@ -8,9 +10,9 @@ describe('AdminMenu', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AdminMenu]
-    })
-    .compileComponents();
+      imports: [AdminMenu, RouterTestingModule],
+      schemas: [NO_ERRORS_SCHEMA] // ignore RouterLink et autres directives inconnues
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AdminMenu);
     component = fixture.componentInstance;
