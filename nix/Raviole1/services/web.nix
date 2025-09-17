bed48c39e4e96dc38fa43e0e13f110056a177847
GaspardCulis <gaspard@gasdev.fr>
2025-09-17 10:53:01 +0200

    fix(nix/web): Fix angular routing failing to work with caddy

--- nix/Raviole1/services/web.nix
+++ nix/Raviole1/services/web.nix
@@ -10,5 +10,13 @@ in {
   services.caddy.virtualHosts."www.${domain}".extraConfig = ''
     root * ${regionoix-frontend}
     file_server
+
+    @notFound {
+      not path /api/*
+      not path /api-docs/*
+      not file {path}
+    }
+
+    rewrite @notFound /index.html
   '';
 }
