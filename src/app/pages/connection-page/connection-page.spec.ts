0edb0651a449e7c229cee49f3452ce4c3330708e
delachal <laurine.delachapelle@gmail.com>
2025-09-17 15:00:31 +0200

    test(front): add tests for basket home connection and create-account pages

--- src/app/pages/connection-page/connection-page.spec.ts
+++ src/app/pages/connection-page/connection-page.spec.ts
@@ -1,23 +1,99 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { of, throwError, BehaviorSubject } from 'rxjs';
+import { Router } from '@angular/router';
 import { ConnectionPage } from './connection-page';
+import { AuthentificationService, LoggedUser, Roles } from '../../generated/clients/regionoix-client';
+import { AuthStateService } from '../../services/auth-state-service';
+import { SnackbarService } from '../../services/snackbar-service';
+import { BasketStateService } from '../../services/basket-state-service';
+import { HttpResponse } from '@angular/common/http';
 
 describe('ConnectionPage', () => {
   let component: ConnectionPage;
   let fixture: ComponentFixture<ConnectionPage>;
+  let routerSpy: jasmine.SpyObj<Router>;
+  let authServiceSpy: jasmine.SpyObj<AuthentificationService>;
+  let authStateSpy: jasmine.SpyObj<AuthStateService>;
+  let snackBarSpy: jasmine.SpyObj<SnackbarService>;
+  let basketStateSpy: jasmine.SpyObj<BasketStateService>;
 
   beforeEach(async () => {
+    routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+    authServiceSpy = jasmine.createSpyObj('AuthentificationService', ['login']);
+    authStateSpy = jasmine.createSpyObj('AuthStateService', ['notifyAuthChanged'], {
+      user$: new BehaviorSubject<any>(null)
+    });
+    snackBarSpy = jasmine.createSpyObj('SnackbarService', ['show']);
+    basketStateSpy = jasmine.createSpyObj('BasketStateService', ['refreshCount']);
+
     await TestBed.configureTestingModule({
-      imports: [ConnectionPage]
-    })
-      .compileComponents();
+      imports: [FormsModule, ConnectionPage],
+      providers: [
+        { provide: Router, useValue: routerSpy },
+        { provide: AuthentificationService, useValue: authServiceSpy },
+        { provide: AuthStateService, useValue: authStateSpy },
+        { provide: SnackbarService, useValue: snackBarSpy },
+        { provide: BasketStateService, useValue: basketStateSpy },
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ConnectionPage);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should validate correct email', () => {
+    component.email = 'test@example.com';
+    expect(component.checkCredentials()).toBeTrue();
+  });
+
+  it('should invalidate incorrect email', () => {
+    component.email = 'invalid-email';
+    expect(component.checkCredentials()).toBeFalse();
+  });
+
+  it('should navigate to create-account page', () => {
+    component.goToCreate();
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['/create-account']);
+  });
+
+  it('should login successfully and navigate based on role', fakeAsync(() => {
+    component.email = 'user@example.com';
+    component.password = 'password';
+
+    const mockLoggedUser: LoggedUser = {
+      id: 1,
+      email: 'user@example.com',
+      role: Roles.Admin
+    };
+    authServiceSpy.login.and.returnValue(of(new HttpResponse({ body: mockLoggedUser })));;
+    const userSubject = new BehaviorSubject<LoggedUser | null>(null);
+    authStateSpy.user$ = userSubject.asObservable();
+    component.onSubmit();
+    userSubject.next(mockLoggedUser);
+    tick();
+
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['/backoffice']);
+  }));
+
+  it('should show error message on failed login', () => {
+    component.email = 'user@example.com';
+    component.password = 'password';
+    authServiceSpy.login.and.returnValue(throwError(() => new Error('fail')));
+
+    component.onSubmit();
+
+    expect(snackBarSpy.show).toHaveBeenCalledWith(
+      'Échec de la connexion. Veuillez vérifier vos identifiants et réessayer.', 'error'
+    );
+  });
+
+  it('should show error for invalid email', () => {
+    component.email = 'invalid-email';
+    component.password = 'password';
+
+    component.onSubmit();
+
+    expect(snackBarSpy.show).toHaveBeenCalledWith('L’adresse e-mail saisie est invalide.', 'error');
   });
 });
--- src/app/pages/create-account/create-account.spec.ts
+++ src/app/pages/create-account/create-account.spec.ts
@@ -1,16 +1,38 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { of, throwError } from 'rxjs';
 import { CreateAccount } from './create-account';
+import { SnackbarService } from '../../services/snackbar-service';
+import { ClientService, LoggedUser, Roles } from '../../generated/clients/regionoix-client';
+import { HttpResponse } from '@angular/common/http';
 
 describe('CreateAccount', () => {
   let component: CreateAccount;
   let fixture: ComponentFixture<CreateAccount>;
+  let routerSpy: jasmine.SpyObj<Router>;
+  let snackBarSpy: jasmine.SpyObj<SnackbarService>;
+  let clientServiceSpy: jasmine.SpyObj<ClientService>;
+
+  const mockLoggedUser: LoggedUser = {
+    id: 1,
+    email: 'john@example.com',
+    role: Roles.Client
+  };
 
   beforeEach(async () => {
+    routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+    snackBarSpy = jasmine.createSpyObj('SnackbarService', ['show']);
+    clientServiceSpy = jasmine.createSpyObj('ClientService', ['register']);
+
     await TestBed.configureTestingModule({
-      imports: [CreateAccount]
-    })
-    .compileComponents();
+      imports: [FormsModule, CreateAccount],
+      providers: [
+        { provide: Router, useValue: routerSpy },
+        { provide: SnackbarService, useValue: snackBarSpy },
+        { provide: ClientService, useValue: clientServiceSpy }
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CreateAccount);
     component = fixture.componentInstance;
@@ -19,5 +41,87 @@ describe('CreateAccount', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show error if form is invalid', () => {
+    const form = { invalid: true } as NgForm;
+    component.onRegister(form);
+    expect(snackBarSpy.show).toHaveBeenCalledWith(
+      'Veuillez remplir correctement tous les champs.', 'error'
+    );
+  });
+
+  it('should show error if passwords do not match', () => {
+    const form = { invalid: false, resetForm: () => { } } as NgForm;
+    component.password = 'password1';
+    component.confirmPassword = 'password2';
+    component.onRegister(form);
+    expect(snackBarSpy.show).toHaveBeenCalledWith(
+      'Les mots de passe ne correspondent pas.', 'error'
+    );
+  });
+
+  it('should show error if password too short', () => {
+    const form = { invalid: false, resetForm: () => { } } as NgForm;
+    component.password = 'short';
+    component.confirmPassword = 'short';
+    component.onRegister(form);
+    expect(snackBarSpy.show).toHaveBeenCalledWith(
+      'Le mot de passe doit contenir au minimum 8 cractères', 'error'
+    );
+  });
+
+  it('should show error if email invalid', () => {
+    const form = { invalid: false, resetForm: () => { } } as NgForm;
+    component.password = 'password123';
+    component.confirmPassword = 'password123';
+    component.email = 'invalid-email';
+    component.onRegister(form);
+    expect(snackBarSpy.show).toHaveBeenCalledWith(
+      'Veuillez saisir une adresse email correct.', 'error'
+    );
+  });
+
+  it('should register successfully and navigate to login', fakeAsync(() => {
+    const form = { invalid: false, resetForm: jasmine.createSpy('resetForm') } as unknown as NgForm;
+
+    component.lastname = 'Doe';
+    component.fisrtname = 'John';
+    component.email = 'john@example.com';
+    component.password = 'password123';
+    component.confirmPassword = 'password123';
+
+    clientServiceSpy.register.and.returnValue(of(new HttpResponse({ body: mockLoggedUser })));
+
+    component.onRegister(form);
+    tick();
+
+    expect(snackBarSpy.show).toHaveBeenCalledWith('Compte créé avec succès !', 'success');
+    expect(form.resetForm).toHaveBeenCalled();
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['/connection']);
+  }));
+
+  it('should show error if registration fails', fakeAsync(() => {
+    const form = { invalid: false, resetForm: () => { } } as NgForm;
+
+    component.lastname = 'Doe';
+    component.fisrtname = 'John';
+    component.email = 'john@example.com';
+    component.password = 'password123';
+    component.confirmPassword = 'password123';
+
+    clientServiceSpy.register.and.returnValue(throwError(() => new Error('fail')));
+
+    component.onRegister(form);
+    tick();
+
+    expect(snackBarSpy.show).toHaveBeenCalledWith(
+      'Une erreur est survenu, veuillez contacter le support', 'error'
+    );
+  }));
+
+  it('should navigate to login page when goToLogin is called', () => {
+    component.goToLogin();
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['/connection']);
   });
 });
