52ea34c89aa77345e70eaffcafee463170dda002
annaclavelier <anna.clavelier@etu.univ-grenoble-alpes.fr>
2025-09-18 11:22:49 +0200

    fix(front): Add to basket on home

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,8 +1,11 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProductsService, ProductDto, CategoryDto } from '../../generated/clients/regionoix-client';
+import { ProductsService, ProductDto, CategoryDto, LoggedUser, BasketService } from '../../generated/clients/regionoix-client';
 import { ProductCardComponent } from '../../utils/component/product-card-component/product-card-component';
 import { Router } from '@angular/router';
+import { AuthStateService } from '../../services/auth-state-service';
+import { SnackbarService } from '../../services/snackbar-service';
+import { BasketStateService } from '../../services/basket-state-service';
 
 @Component({
   selector: 'app-home-page',
@@ -12,12 +15,20 @@ import { Router } from '@angular/router';
   styleUrl: './home-page.css',
 })
 export class HomePage implements OnInit {
+  private readonly snackbar = inject(SnackbarService);
+
   private readonly productService = inject(ProductsService);
+  private readonly authStateService = inject(AuthStateService);
+  private readonly basketService = inject(BasketService);
+  private readonly basketState = inject(BasketStateService);
+
   private readonly router = inject(Router);
   promotionalProducts: ProductDto[] = [];
   newProducts: ProductDto[] = [];
   bestProducts: ProductDto[] = [];
   topCategories: CategoryDto[] = [];
+  user: null | LoggedUser = null;
+
 
   currentIndex = 0;
 
@@ -27,6 +38,25 @@ export class HomePage implements OnInit {
     this.loadPromotionalProducts();
     this.loadNewProducts();
     this.loadBestProducts();
+    this.basketState.refreshCount();
+    this.user = this.authStateService.currentUser;
+  }
+
+  addItem(productId: number) {
+    const user = this.authStateService.currentUser;
+    if (!user) {
+      this.snackbar.show('Vous devez vous connecter pour ajouter au panier.', 'info');
+      return;
+    }
+    this.basketService.add({ product_id: productId, quantity: 1 }).subscribe({
+      next: () => {
+        this.snackbar.show('Produit ajouté au panier ✅', 'success');
+        this.basketState.refreshCount();
+      },
+      error: () => {
+        this.snackbar.show('Stock insuffisant !', 'error');
+      }
+    });
   }
 
   loadPromotionalProducts() {
