30d7615e0e1f96b2ebb8cfab12d5fd31ed92ff7d
Zakaria <zakaria.bentanfous@gmail.com>
2025-09-18 10:13:53 +0200

    feat(front): add basket not empty guard in connection page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,6 +14,9 @@ import { FormProduct } from './pages/form-product/form-product';
 import { HomePage } from './pages/home-page/home-page';
 import { BackofficeProduct } from './pages/backoffice-product/backoffice-product';
 import { CreateAccount } from './pages/create-account/create-account';
+import { PaymentError } from './pages/payment-error/payment-error';
+import { PaymentSuccessful } from './pages/payment-successful/payment-successful';
+import { BasketNotEmptyGuard } from './services/basket-not-empty-guard';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -23,10 +26,12 @@ export const routes: Routes = [
     { path: 'backoffice/products', component: BackofficeProducts, canActivate: [AdminGuard] },
     { path: 'backoffice/create-product', component: FormProduct, canActivate: [AdminGuard] },
     { path: 'backoffice/products/:id', component: BackofficeProduct, canActivate: [AdminGuard] },
-    { path: 'payment', component: PaymentPage, canActivate: [AuthGuard] },
+    { path: 'payment', component: PaymentPage, canActivate: [AuthGuard, BasketNotEmptyGuard] },
     { path: 'connection', component: ConnectionPage, canActivate: [NoAuthGuard] },
     { path: 'profile', component: ProfilePage, canActivate: [AuthGuard] },
     { path: 'products/:id', component: ProductPage },
     { path: 'home', component: HomePage },
-    { path: 'create-account', component: CreateAccount, canActivate: [NoAuthGuard] }
+    { path: 'create-account', component: CreateAccount, canActivate: [NoAuthGuard] },
+    { path: 'error-payment', component: PaymentError },
+    { path: 'payment-successful', component: PaymentSuccessful }
 ];
